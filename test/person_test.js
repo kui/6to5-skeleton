c86ff26552ba86c079550322968aa51e7d680867
Keiichiro Ui <keiichiro.ui@gmail.com>
2015-02-01 19:13:17 +0900

    Mod sample failure test

--- test/person_test.js
+++ test/person_test.js
@@ -3,7 +3,7 @@ import {Person} from '../src/person';
 
 describe('Person', () => {
   let alice = new Person("alice", 12);
-  it("#mistake", () => {
-    assert(alice.name.length === 4);
+  it("#fail", () => {
+    assert(alice.toString().length === 4);
   });
 });
